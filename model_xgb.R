628f67ddc149b030dcc6855f9ad69432eedc9aae
aleksejhoffaerber <aleksejhoffaerber@users.noreply.github.com>
2021-06-04 14:40:28 +0200

    FI XGB (in-sample)

--- model_xgb.R
+++ model_xgb.R
@@ -19,7 +19,7 @@ to_model <- lifetime_clean %>%
   select(-installation_dttm)
 
 # data splits
-splits <-  initial_split(to_model, strata = `35`, prop = 0.3)
+splits <-  initial_split(to_model, strata = `35`, prop = 0.7)
 train <- training(splits)
 test <- testing(splits)
 
@@ -34,11 +34,11 @@ recipe_xgb <- train %>%
 # cv splits
 cv_folds <- bake(recipe_xgb,
                  new_data = training(splits)) %>% 
-  rsample::vfold_cv(v = 10) # TODO: or 10?
+  rsample::vfold_cv(v = 10)
 
 # model specification
 model_xgb <- boost_tree(mode = "regression",
-                        trees = 500,
+                        trees = 500, # FIXME
                         min_n = tune(),
                         tree_depth = tune(),
                         learn_rate = tune(),
@@ -80,7 +80,7 @@ final_xgb <- tuned_xgb %>%
 model_final_xgb <- model_xgb %>% 
   finalize_model(final_xgb)
 
-# train set fit
+# train set fit -----
 train_processed <- bake(recipe_xgb,  new_data = training(splits))
 train_prediction <- model_final_xgb %>%
   # fit the model on all the training data
@@ -88,40 +88,38 @@ train_prediction <- model_final_xgb %>%
     formula = `35` ~ ., 
     data    = train_processed
   )
-# # predict the sale prices for the training data
-# predict(new_data = train_processed) %>%
-# bind_cols(training(splits))
 
-# feature importance for most important features
+
+# in-sample feature importance for most important features -----
 xgboost::xgb.importance(model = train_prediction$fit) %>% head(n = 10)
 
 
 
 
-# test set fit
+# test set fit -----
 test_processed  <- bake(recipe_xgb, new_data = testing(splits))
 test_prediction <- model_final_xgb %>%
-  # fit the model on all the training data
   fit(
     # select most important features
-    formula = `35` ~ lifetime + `26` + `13` + `39` + `12` + `51` + `53` + `22` + `33` + `45`, 
+    formula = `35` ~ lifetime + `26` + `44`, 
+    # + `31` + `13`,
+    # `33` + `5` + `2` + `49` + `14`, 
     data    = train_processed
   ) %>%
-  # use the training model fit to predict the test data
   predict(new_data = test_processed) %>%
   bind_cols(testing(splits))
-# measure the accuracy of our model using `yardstick`
 
-xgboost_score <- 
+# out-of-sample accuracy  -----
+metrics_xgb <- 
   test_prediction %>%
   yardstick::metrics(`35`, .pred) %>%
-  mutate(.estimate = format(round(.estimate, 2), big.mark = ","))
+  mutate(.estimate = format(round(.estimate, 3), big.mark = ","))
 
-test_prediction %>% 
-  select(.pred, `35`, lifetime) %>% 
-  ggplot(aes(`35`, .pred)) + 
-  geom_point() # TODO: plot together with lifetime
 
+
+# GRAPHS -----
+
+# predicted vs. actual sensor data for sensor 35
 test_prediction %>% 
   select(.pred, `35`, lifetime) %>%
   rename(Prediction = .pred,
@@ -130,9 +128,32 @@ test_prediction %>%
                names_to = "type",
                values_to ="value") %>% 
   ggplot(aes(lifetime, value, colour = type)) + 
-  geom_line(alpha = .3) + # TODO: find a way to smoothe it
+  geom_line(alpha = .5) + 
   geom_smooth(se = F) +
+  scale_x_continuous(breaks = seq(0,900,50)) +
+  scale_y_continuous(limits = c(0.02,0.125)) +
+  scale_colour_manual(values = c("Actual" = "gray30",
+                                 "Prediction" = "#0533ff")) +
+  labs(title = "Model fit using 3 most important features",
+       x = "Lifetime",
+       y = "Sensor 35 Values") +
   theme_bw() +
   theme(legend.position = "bottom", legend.box = "horizontal") +
-  scale_color_discrete(NULL) + 
   guides(colour = guide_legend(nrow = 1))
+  
+ggsave("06_XGB_Fit Results.png", path = "Plots", 
+         width = 7, height = 6, dpi = 300)
+
+# feature importance for XGB
+xgboost::xgb.importance(model = train_prediction$fit) %>% head(n = 10) %>% 
+  ggplot(aes(Gain, reorder(Feature, Gain))) +
+  geom_col() +
+  labs(title = "Top 10 features",
+       x = "Explanatory Gain",
+       y = "Feature") +
+  theme_bw() 
+
+ggsave("07_XGB_FI Results.png", path = "Plots", 
+       width = 3, height = 6, dpi = 300)
+  
+
