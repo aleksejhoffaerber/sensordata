0aff7522d2f92401511b2d3ea8849761e4817390
Aleksej Hoffarber <aleksej.hoffarber@houston-analytics.com>
2021-10-06 14:43:42 +0200

    ALE script

--- interpretability.R
+++ interpretability.R
@@ -50,4 +50,50 @@ to_ice_plot %>%
 ggsave("08_ICE XGBoost Model.png", path = "Plots", 
       width = 10, height = 6, dpi = 300)
 
+# build ALE plots 
+
+# create training data in format
+to_ale <- train %>% 
+  mutate_if(is.character, as.factor)
+
+feature_classes <- to_ale %>% 
+  summarise_all(class) %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  rownames_to_column() %>% 
+  rename(feature = 1, class = 2) %>% 
+  mutate(row_number = row_number())
+
+numerical_features <- feature_classes %>% 
+  filter(class == "numeric") %>% 
+  pull(row_number)
+
+# normally filtering out, but all classes are numeric 
+xgb_model <- finalize_workflow(workflow_xgb,
+                               tuned_xgb %>% 
+                                 select_best()) %>% 
+  fit(train)
+
+# create ALE
+tic_ale <- Sys.time()
+ale <- map(
+  numerical_features,
+  ~safely(custom_ale)(X = as.data.frame(to_ale),
+                      X.model = xgb_model,
+                      pred.fun = predict_tidymodels,
+                      J = .x,
+                      K = 100)$result %>% 
+    as_tibble() %>% 
+    mutate(feature = colnames(to_ale)[.x]) %>% 
+    select(feature, everything())) %>% 
+  bind_rows()
+
+# TODO: try out with previous finals, especially final_workflow_xgb  
+# does not need the actual final model (model_final_xgb), which contains the final parameters
+# does not need the final model and its fit (so model_final_xgb %>% fit + formlula etc.)
+# does not need final_workflow_xgb because we need a pure workflow, not a a full definition of recipe etc.
+
+(toc_ale <- Sys.time() - tic_ale)
+
+# plot ALE
 
